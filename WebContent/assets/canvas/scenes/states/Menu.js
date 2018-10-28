40a39f33b6bccad0201de2306290febb08feeb09
Jordan Machalek <jdm4344@rit.edu>
2018-10-28 00:09:52 -0400

    Finally figured out how to make a button work and show/hide things

--- WebContent/assets/canvas/scenes/states/Menu.js
+++ WebContent/assets/canvas/scenes/states/Menu.js
@@ -35,10 +35,6 @@ class Menu extends Phaser.State {
 		var _s_colored_desert = this.add.sprite(-4.0, -4.0, 'colored_desert');
 		_s_colored_desert.scale.setTo(1.0104976500063336, 0.7569919450115183);
 		
-		var _instructionPanel = new instructionPanel(this.game);
-		_instructionPanel.position.setTo(250.0, 125.0);
-		
-		
 		var _startButton = this.add.group();
 		
 		this.add.button(450.0, 550.0, 'red_button01', function(){this.game.state.start("Level")}, this, null, null, null, null, _startButton);
@@ -51,15 +47,30 @@ class Menu extends Phaser.State {
 		
 		this.add.text(850.0, 710.0, 'Instructions	', {"font":"bold 20px Arial"}, _instructionsButton);
 		
+		var _instructionsPanel = this.add.group();
 		
+		var _s_instructions_panel = this.add.sprite(250.0, 125.0, 'red_panel', null, _instructionsPanel);
+		_s_instructions_panel.scale.setTo(6.0, 6.0);
+		
+		this.add.text(309.0, 177.0, 'Instructions:\n\n• Don\'t let your citizens die\n\nGood Luck Out there!', {"font":"bold 20px Arial"}, _instructionsPanel);
+		
+		var _instructionPanel = new instructionPanel(this.game);
+		_instructionPanel.position.setTo(251.0, 126.0);
+		
+		var _b_instructions1 = new instructionButton(this.game, 386.0, 662.0);
+		this.add.existing(_b_instructions1);
+		
+		_b_instructions1.setInstructionPanel(_instructionPanel);
 		
 		
 	}
 	
 	/* state-methods-begin */
 	// -- user code here --
+	//_b_instructions1.setInstructionPanel(_s_instructions_panel);
 	/* state-methods-end */
 	
 }
 /* --- end generated code --- */
 	// -- user code here --
+console.log(game);
--- WebContent/js/Main.js
+++ WebContent/js/Main.js
@@ -1,6 +1,7 @@
+var game;
 
 window.onload = function() {
-	var game = new Phaser.Game(1024, 768, Phaser.AUTO, '', null, false, true, {"arcade":true});
+	game = new Phaser.Game(1024, 768, Phaser.AUTO, '', null, false, true, {"arcade":true});
 
 	game.state.add("Menu", Menu);
 	game.state.add("Level", Level);
