4cd51a006f548d63d4f48c9fb502d4e198b295d4
Dennis Dennis <dennis.slavinsky@gmail.com>
2018-10-28 04:34:00 -0400

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of
    https://github.com/DennisSSDev/Fallout-Shelter-Online.git
    
    Conflicts:
    	.settings/org.eclipse.core.resources.prefs
    	WebContent/assets/pack.json

--- WebContent/assets/canvas/scenes/states/Menu.js
+++ WebContent/assets/canvas/scenes/states/Menu.js
@@ -35,29 +35,33 @@ class Menu extends Phaser.State {
 		var _s_colored_desert = this.add.sprite(-4.0, -4.0, 'colored_desert');
 		_s_colored_desert.scale.setTo(1.0104976500063336, 0.7569919450115183);
 		
-		var _instructionPanel = new instructionPanel(this.game);
-		_instructionPanel.position.setTo(250.0, 125.0);
-		
-		
 		var _startButton = this.add.group();
 		
 		this.add.button(450.0, 550.0, 'red_button01', function(){this.game.state.start("Level")}, this, null, null, null, null, _startButton);
 		
 		this.add.text(480.0, 560.0, 'Click to Start', {"font":"bold 20px Arial"}, _startButton);
 		
-		var _instructionsButton = this.add.group();
+		var _instructionsPanel = this.add.group();
 		
-		this.add.button(815.0, 700.0, 'red_button01', null, this, null, null, null, null, _instructionsButton);
+		var _s_instructions_panel = this.add.sprite(250.0, 125.0, 'red_panel', null, _instructionsPanel);
+		_s_instructions_panel.scale.setTo(6.0, 6.0);
 		
-		this.add.text(850.0, 710.0, 'Instructions	', {"font":"bold 20px Arial"}, _instructionsButton);
+		var _t_instructions = this.add.text(309.0, 177.0, 'Instructions:\n\n• Don\'t let your citizens die\n\nGood Luck Out there!', {"font":"bold 20px Arial"}, _instructionsPanel);
+		
+		var _b_instructions = new instructionButton(this.game, 815.0, 700.0);
+		this.add.existing(_b_instructions);
+		
+		this.add.text(850.0, 710.0, 'Instructions	', {"font":"bold 20px Arial"});
 		
 		
+		_b_instructions.setInstructionPanel(_s_instructions_panel, _t_instructions);
 		
 		
 	}
 	
 	/* state-methods-begin */
 	// -- user code here --
+	//_b_instructions1.setInstructionPanel(_s_instructions_panel);
 	/* state-methods-end */
 	
 }
